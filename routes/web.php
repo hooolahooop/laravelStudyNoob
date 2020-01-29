1a73ef83759e8d0c55424b4ca542c0659bfd5c2f
Artem <tcslogist@gmail.com>
2020-01-29 14:53:22 +0300

    CRUD link added & auth upgrade

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('tasks', 'TaskController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
